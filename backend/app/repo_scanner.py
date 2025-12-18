ed7e946e319a4a4e4425ec4f2d1cf902d3964fac
Vicky404-git <dhruwv499@gmail.com>
2025-12-18 05:50:15 +0530

    megatest back to 1 again 'lol' :)

--- backend/app/repo_scanner.py
+++
@@ -1,9 +0,0 @@
-from git import Repo
-import os
-
-Repo.clone_from(repo_url, "./repos/project1")
-
-for root, dirs, files in os.walk("./repos/project1"):
-    for file in files:
-        if file.endswith((".js", ".py", ".ts")):
-            print(os.path.join(root, file))
--- test.py
+++ test.py
@@ -1,3 +1,4 @@
 AWS_KEY = "AKIAEXAMPLE123456789"  # Rule 1.1
 admin_pass = "password123!"     # Rule 1.6
+user_input = input()
 result = eval("calculate(" + user_input + ")")  # Rule 2.1
